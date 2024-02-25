452f5d4ad2574cae3178d19c4377b7d0ee39aca7
Snowkai <34677392+Snowkai@users.noreply.github.com>
2024-02-25 22:46:19 +0300

    Create simple AI

--- Assets/Script/RayShooter.cs
+++ Assets/Script/RayShooter.cs
@@ -9,6 +9,9 @@ public class RayShooter : MonoBehaviour
     void Start()
     {
         _camera = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -21,14 +24,29 @@ public class RayShooter : MonoBehaviour
             RaycastHit hit;
             if(Physics.Raycast(ray, out hit))
             {
-                StartCoroutine(SphereIndicator(hit.point));
+                GameObject hitObject = hit.transform.gameObject;
+                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+                if(target!=null){
+                    target.ReactToHit();
+                } else{
+                    StartCoroutine(SphereIndicator(hit.point));
+                }
             }
         }
+    }
+
+    void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth/2 - size/4;
+        float posY = _camera.pixelHeight/2 - size/2;
+        GUI.Label(new Rect(posX, posY, size, size),"*");
     }
 
     private IEnumerator SphereIndicator(Vector3 pos)
     {
         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.4f,0.4f,0.4f);
         sphere.transform.position = pos;
         yield return new WaitForSeconds(1);
         Destroy(sphere);
